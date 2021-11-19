a3ac92d0dbb45db4c5853002f0ea0cc81ad44e54
IsraMarquez <49415210+IsraMarquez@users.noreply.github.com>
2021-11-18 22:40:13 -0600

    Mejora en el flujo

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -13,23 +13,30 @@ public class MenuController : MonoBehaviour, IPointerEnterHandler, IPointerClick
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("Clicked!");
-        Location2();
+        //Debug.Log("Clicked!");
+        //Location2();
     }
-    public GameObject camera;
-    public GameObject uiCanvas;
-    public GameObject startPoint;
-    public GameObject startPoint2;
+    //public GameObject camera;
+    //public GameObject uiCanvas;
+    //public GameObject startPoint;
+    //public GameObject startPoint2;
 
-    public GameObject brain;
+    public GameObject[] organs;
+    /*public GameObject brain;
     public GameObject higado;
     public GameObject pancreas;
+    public GameObject intestino;
+    public GameObject estomago;
+    public GameObject musculo;
+    public GameObject adiposo;*/
 
-    public GameObject pnl_ui;
-    public GameObject btn_continue;
-    public GameObject btn_back;
+    public GameObject pnl_menu;
+    public GameObject[] pnl_menuDetail;
+    public GameObject pnl_Exit;
+    public GameObject moleculas;
+    //public GameObject btn_back;
 
-    Vector3 startPoint2Canvas;
+    //Vector3 startPoint2Canvas;
 
     // Start is called before the first frame update
     void Start()
@@ -49,37 +56,137 @@ public class MenuController : MonoBehaviour, IPointerEnterHandler, IPointerClick
         //uiCanvas.transform.position = new Vector3(startPoint2.transform.position.x, startPoint2.transform.position.y, startPoint2.transform.position.z + 7);
     }
 
-    public void HideAll()
+    public void HideAllOrgans()
+    {
+        for(int i=0; i<organs.Length; i++)
+        {
+            organs[i].SetActive(false);
+        }
+    }
+
+    public void HidePanelDetails()
+    {
+        for(int i=0; i<pnl_menuDetail.Length; i++)
+        {
+            pnl_menuDetail[i].SetActive(false);
+        }
+    }
+
+    public void ShowOrgan(int _organ)
+    {
+        HideAllOrgans();
+        HidePanelDetails();
+        pnl_menu.SetActive(false);
+        moleculas.SetActive(false);
+        
+        pnl_menuDetail[_organ].SetActive(true);
+        organs[_organ].SetActive(true);
+    }
+
+    public void Repeat(int _organ)
+    {
+        //StopAllSounds
+        /*HideAllOrgans();
+        HidePanelDetails();
+        pnl_menu.SetActive(false);
+        moleculas.SetActive(false);
+        
+        pnl_menuDetail[_organ].SetActive(true);
+        organs[_organ].SetActive(true);*/
+    }
+
+    /*public void HideAllOrgans()
     {
         brain.SetActive(false);
         higado.SetActive(false);
         pancreas.SetActive(false);
-        pnl_ui.SetActive(false);
     }
 
     public void ShowBrain()
     {
-        HideAll();
-        btn_back.SetActive(true);
+        HideAllOrgans();
+        pnl_menu.SetActive(false);
+        pnl_menuDetail.SetActive(true);
+        //btn_back.SetActive(true);
         brain.SetActive(true);
     }
 
     public void ShowHigado()
     {
-        HideAll();
-        btn_back.SetActive(true);
+        HideAllOrgans();
+        pnl_menu.SetActive(false);
+        pnl_menuDetail.SetActive(true);
+        //btn_back.SetActive(true);
         higado.SetActive(true);
     }
 
     public void ShowPancreas()
     {
-        HideAll();
-        btn_back.SetActive(true);
+        HideAllOrgans();
+        pnl_menu.SetActive(false);
+        pnl_menuDetail.SetActive(true);
+        //btn_back.SetActive(true);
         pancreas.SetActive(true);
     }
+
+    public void ShowIntestino()
+    {
+        HideAllOrgans();
+        pnl_menu.SetActive(false);
+        pnl_menuDetail.SetActive(true);
+        //btn_back.SetActive(true);
+        intestino.SetActive(true);
+    }
+
+    public void ShowEstomago()
+    {
+        HideAllOrgans();
+        pnl_menu.SetActive(false);
+        pnl_menuDetail.SetActive(true);
+        //btn_back.SetActive(true);
+        estomago.SetActive(true);
+    }
+
+    public void ShowMusculo()
+    {
+        HideAllOrgans();
+        pnl_menu.SetActive(false);
+        pnl_menuDetail.SetActive(true);
+        //btn_back.SetActive(true);
+        musculo.SetActive(true);
+    }
+
+    public void ShowAdiposo()
+    {
+        HideAllOrgans();
+        pnl_menu.SetActive(false);
+        pnl_menuDetail.SetActive(true);
+        //btn_back.SetActive(true);
+        adiposo.SetActive(true);
+    }*/
+
     public void back()
     {
-        pnl_ui.SetActive(true);
-        btn_back.SetActive(false);
+        HideAllOrgans();
+        HidePanelDetails();
+        pnl_menu.SetActive(true);
+        moleculas.SetActive(true);
+        //pnl_menuDetail.SetActive(false);
+        //btn_back.SetActive(false);
+    }
+
+    public void ShowSureExit()
+    {
+        pnl_Exit.SetActive(true);
+    }
+
+    public void HideSureExit()
+    {
+        pnl_Exit.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
